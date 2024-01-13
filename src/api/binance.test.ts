dd83a89ea65876ffe645e8f240ea3bf9f8d74c22
Florian Goussin <floriangoussin@floriangoussin.local>
2024-01-13 05:48:31 -0700

    write a simple test

--- src/api/binance.test.ts
+++ src/api/binance.test.ts
@@ -6,15 +6,62 @@ import {
   // getTrades
 } from './binance';
 
-// import type {
-//   Ticker24Response,
-//   TradesResponse
-// } from './binance-types';
+import {
+  type Ticker24Response,
+  // type TradesResponse
+} from './binance-types';
 
 const domain = 'http://fakeapi.com/';
 vi.mock('axios');
 const mockedAxios = axios as Mocked<typeof axios>;
 const mockedConsole = vi.spyOn(global.console, 'error');
+
+const mockedDataOne: Ticker24Response = {
+  symbol: 'symbol1',
+  priceChange: 'string',
+  priceChangePercent: 'string',
+  weightedAvgPrice: 'string',
+  prevClosePrice: 'string',
+  lastPrice: 'string',
+  lastQty: 'string',
+  bidPrice: 'string',
+  bidQty: 'string',
+  askPrice: 'string',
+  askQty: 'string',
+  openPrice: 'string',
+  highPrice: 'string',
+  lowPrice: 'string',
+  volume: 'string',
+  quoteVolume: 'string',
+  openTime: new Date().toDateString(),
+  closeTime: new Date().toDateString(),
+  firstId: 23341,
+  lastId: 67533,
+  count: 98735
+}
+const mockedDataTwo: Ticker24Response = {
+  symbol: 'symbol2',
+  priceChange: 'string',
+  priceChangePercent: 'string',
+  weightedAvgPrice: 'string',
+  prevClosePrice: 'string',
+  lastPrice: 'string',
+  lastQty: 'string',
+  bidPrice: 'string',
+  bidQty: 'string',
+  askPrice: 'string',
+  askQty: 'string',
+  openPrice: 'string',
+  highPrice: 'string',
+  lowPrice: 'string',
+  volume: 'string',
+  quoteVolume: 'string',
+  openTime: new Date().toDateString(),
+  closeTime: new Date().toDateString(),
+  firstId: 23341,
+  lastId: 67533,
+  count: 98735
+}
 
 describe('Binance API tests', () => {
   afterEach(() => {
@@ -26,15 +73,19 @@ describe('Binance API tests', () => {
     mockedConsole.mockReset()
   })
 
-  it('Should get api data', async () => {
+  it('Should get data from the api', async () => {
     mockedAxios.get.mockResolvedValueOnce({
-      data: [{
-        test: 'Hi I worked!',
-        openTime: new Date().toDateString(),
-        closeTime: new Date().toDateString()
-      }]
+      data: [
+        mockedDataOne,
+        mockedDataTwo
+      ]
+    });
+    const data = await getTicker24(domain);
+    expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+    expect(data[0]).toEqual(expect.objectContaining({
+      symbol: mockedDataOne.symbol
     })
-    await getTicker24(domain)
-    expect(mockedAxios.get).toHaveBeenCalledTimes(1)
+    );
   })
+
 });
